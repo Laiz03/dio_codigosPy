1755b4cfcc2fe9fedf5a8ed0fcd7684b9c8442ec
Laiz03 <laizoliveira891@gmail.com>
2024-12-17 21:19:59 -0300

    Primeiro commit

---
+++ concat_dados.py
@@ -0,0 +1,8 @@
+#receber dois dados e concatela-nos 
+
+info1 = input("Digite o primeira informação: ")
+info2 = input("Digite a segunda informação: ")
+
+info_concatenacao = info1 + "" + info2
+
+print("As informações concatenadas são: ", info_concatenacao)
---
+++ repet.py
@@ -0,0 +1,6 @@
+# recebe um numero e um texto e retorna sendo o número a quantidade de vezes que o texto será retornado
+
+string = input("Digite um texto: ")
+num = int(input("Insira o número: "))
+
+print ((string + " ") * num)
